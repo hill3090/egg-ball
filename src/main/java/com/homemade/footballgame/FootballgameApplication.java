405aaf8f34da82a73b5ca46616a976ad37d2a6ea
Andrew Hill <hill3090@gmail.com>
2021-01-07 20:05:37 -0500

    Initial Commit

---
+++ src/main/java/com/homemade/footballgame/FootballgameApplication.java
@@ -0,0 +1,13 @@
+package com.homemade.footballgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballgameApplication.class, args);
+	}
+
+}
